d857a842d7a3730934276d8f4e1d3c7cee7da42a
jeanvitorvieira <jeanvitorv0@gmail.com>
2026-03-28 04:13:48 -0300

    [feature] starting the project

---
+++ src/main/java/com/jean/fipe_search/FipeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.jean.fipe_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeSearchApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FipeSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jean/fipe_search/FipeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jean.fipe_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
